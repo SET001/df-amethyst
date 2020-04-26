90d32c60421286ca34573ccf37abdfb4ec7f1d42
SET001 <settydark@gmail.com>
2020-04-26 04:40:43 +0300

    up

--- src/menu.rs
+++ src/menu.rs
@@ -1,65 +1,71 @@
-use amethyst::assets::AssetStorage;
 use amethyst::core::transform::Transform;
 use amethyst::window::ScreenDimensions;
 use amethyst::{
-    assets::Handle,
-    prelude::*,
-    renderer::{
-        camera::{Camera, Projection},
-        SpriteRender, SpriteSheet, Texture, Transparent,
-    },
+  assets::Handle,
+  prelude::*,
+  renderer::{
+    camera::{Camera, Projection},
+    SpriteRender, SpriteSheet, Transparent,
+  },
 };
+
+use amethyst::input::{is_key_down, VirtualKeyCode};
 #[derive(Clone)]
 pub struct MenuState {
-    pub background_texture_handle: Handle<SpriteSheet>,
+  pub background_texture_handle: Handle<SpriteSheet>,
 }
 
 impl SimpleState for MenuState {
-    fn handle_event(
-        &mut self,
-        _data: StateData<'_, GameData<'_, '_>>,
-        _event: StateEvent,
-    ) -> SimpleTrans {
+  fn handle_event(&mut self, _: StateData<'_, GameData<'_, '_>>, event: StateEvent) -> SimpleTrans {
+    if let StateEvent::Window(event) = event {
+      if is_key_down(&event, VirtualKeyCode::Escape) {
+        Trans::Quit
+      } else {
         Trans::None
+      }
+    } else {
+      Trans::None
     }
-    fn on_start(&mut self, mut data: StateData<'_, GameData<'_, '_>>) {
-        println!("menu opened");
-        let dimensions = (*data.world.read_resource::<ScreenDimensions>()).clone();
-        init_camera(data.world, &dimensions);
-        initialize_sprite(&mut data.world, self.background_texture_handle.clone());
-    }
+  }
+
+  fn on_start(&mut self, mut data: StateData<'_, GameData<'_, '_>>) {
+    println!("menu opened");
+    let dimensions = (*data.world.read_resource::<ScreenDimensions>()).clone();
+    init_camera(data.world, &dimensions);
+    initialize_sprite(&mut data.world, self.background_texture_handle.clone());
+  }
 }
 
 fn init_camera(world: &mut World, dimensions: &ScreenDimensions) {
-    let mut transform = Transform::default();
-    transform.set_translation_xyz(dimensions.width() * 0.5, dimensions.height() * 0.5, 10.0);
-    let camera = Camera::from(Projection::orthographic(
-        -dimensions.width() / 2.,
-        dimensions.width() / 2.,
-        -dimensions.height() / 2.,
-        dimensions.height() / 2.,
-        0.,
-        200.,
-    ));
-    world.create_entity().with(camera).with(transform).build();
+  let mut transform = Transform::default();
+  transform.set_translation_xyz(dimensions.width() * 0.5, dimensions.height() * 0.5, 10.0);
+  let camera = Camera::from(Projection::orthographic(
+    -dimensions.width() / 2.,
+    dimensions.width() / 2.,
+    -dimensions.height() / 2.,
+    dimensions.height() / 2.,
+    0.,
+    200.,
+  ));
+  world.create_entity().with(camera).with(transform).build();
 }
 
 fn initialize_sprite(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
-    let (width, height) = {
-        let dim = world.read_resource::<ScreenDimensions>();
-        (dim.width(), dim.height())
-    };
-    let mut sprite_transform = Transform::default();
-    sprite_transform.set_translation_xyz(width / 2., height / 2., 0.);
+  let (width, height) = {
+    let dim = world.read_resource::<ScreenDimensions>();
+    (dim.width(), dim.height())
+  };
+  let mut sprite_transform = Transform::default();
+  sprite_transform.set_translation_xyz(width / 2., height / 2., 0.);
 
-    let sprite_render = SpriteRender {
-        sprite_sheet: sprite_sheet_handle,
-        sprite_number: 0, // First sprite
-    };
-    world
-        .create_entity()
-        .with(sprite_render)
-        .with(sprite_transform)
-        .with(Transparent)
-        .build();
+  let sprite_render = SpriteRender {
+    sprite_sheet: sprite_sheet_handle,
+    sprite_number: 0, // First sprite
+  };
+  world
+    .create_entity()
+    .with(sprite_render)
+    .with(sprite_transform)
+    .with(Transparent)
+    .build();
 }
