c4cffa331aa1f30aa6ee4ff2f29e520466a33500
SET001 <settydark@gmail.com>
2020-07-12 15:25:02 +0300

    cleanups for ScrollerSystem

--- src/system/scroller.rs
+++ src/system/scroller.rs
@@ -3,27 +3,20 @@ use amethyst::{
   ecs::{Entities, Join, LazyUpdate, Read, ReadStorage, System, WriteStorage},
 };
 
-use crate::component::{Dimensions, Position, Scroller, ScrollerItem, Velocity};
+use crate::component::{Dimensions, Scroller, Velocity};
 
 pub struct ScrollerSystem;
 
 impl<'a> System<'a> for ScrollerSystem {
   type SystemData = (
     ReadStorage<'a, Dimensions>,
-    ReadStorage<'a, Position>,
     WriteStorage<'a, Scroller>,
-    ReadStorage<'a, ScrollerItem>,
     Entities<'a>,
     Read<'a, LazyUpdate>,
   );
 
-  fn run(
-    &mut self,
-    (dimensions, positions, mut scrollers, scrollersItems, entities, updater): Self::SystemData,
-  ) {
-    for (entity, mut scroller, position, dimension) in
-      (&entities, &mut scrollers, &positions, &dimensions).join()
-    {
+  fn run(&mut self, (dimensions, mut scrollers, entities, updater): Self::SystemData) {
+    for (scroller, dimension) in (&mut scrollers, &dimensions).join() {
       let itemsWidth: f32 = scroller
         .items
         .iter()
@@ -54,24 +47,3 @@ impl<'a> System<'a> for ScrollerSystem {
     }
   }
 }
-//   println!(
-//     "uuid: {}, x: {}, width: {}",
-//     scroller.uuid, position.x, dimension.width
-//   );
-// println!("{:?}", items.len)
-// // for () in (&scroller.item)
-// // scroller.tiles.len();
-// // let b = scroller.tiles.pop().unwrap();
-// // let a = b.sprite_sheet.a;
-// // let entity = entities.create();
-// // let transform = Transform::default();
-// // let x = dimensions.width() - 1920.0;
-// // transform.set_translation_xyz(-x / 2.0, 0., 0.);
-// // updater.insert(entity, transform);
-// // updater.insert(entity, Transparent);
-// // transform.move_right(scroller.speed);
-// // println!("{:?}", dimentions.width());
-// // if transform.translation().x < (scroller.tileWidth * -1.0) {
-// //   println!("{:?}", transform.translation().x)
-// // }
-// // if(transform.)
